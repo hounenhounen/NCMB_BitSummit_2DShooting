a03977890d3348289cf4970b083b83a2e86e09fd
hounenhounen <hounen.hounen@gmail.com>
2015-07-11 13:27:57 +0900

    Update savescore.cs

--- savescore.cs
+++ savescore.cs
@@ -1,37 +1,17 @@
-using NCMB;
+using NCMB; //mobile backendのSDKを読み込む
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class LeaderBoard {
-	
-	public int currentRank = 0;
-	public List<NCMB.Rankers> topRankers = null;
-	
-	// サーバーからトップ5を取得 ---------------    
-	public void fetchTopRankers()
-	{
-		// データストアの「Score」クラスから検索
-
-		NCMBQuery<NCMBObject> query = new NCMBQuery<NCMBObject> ("Score");
-		query.OrderByDescending ("score");
-		query.Limit = 5;
-
-		query.FindAsync ((List<NCMBObject> objList ,NCMBException e) => {
-			
-			if (e != null) {
-				//検索失敗時の処理
-			} else {
-				//検索成功時の処理
-				List<NCMB.Rankers> list = new List<NCMB.Rankers>();
-				// 取得したレコードをscoreクラスとして保存
-				foreach (NCMBObject obj in objList) {
-					int    s = System.Convert.ToInt32(obj["score"]);
-					string n = System.Convert.ToString(obj["name"]);
-					list.Add( new Rankers( s, n ) );
-				}
-				topRankers = list;
-			}
-		});
+public class SaveScore : MonoBehaviour {
+	// mobile backendに接続------------------------
+	public void save( string name, int score ) {
+		NCMBObject obj = new NCMBObject ("Score");
+		obj.Add ("name", name);//オブジェクトに名前とスコアを設定
+		obj.Add ("score", score);
+		obj.SaveAsync ((NCMBException e) => {      
+			if (e != null) {//エラー処理
+			} else {//成功時の処理
+			}                   });//この処理でサーバーに書き込む
 	}
-
 }
